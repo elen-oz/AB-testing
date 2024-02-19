c1d3933c5efbaf041656f6b6e94ef8d48e49aedd
elen-oz <ozerova.lenka@gmail.com>
2024-02-19 23:22:08 +0100

    first comment

---
+++ src/App.tsx
@@ -0,0 +1,193 @@
+import { useState } from 'react';
+
+interface FormData {
+  firstName: string;
+  lastName: string;
+  petChoice: string;
+  petChoice2: string;
+}
+
+const App = () => {
+  const [formData, setFormData] = useState<FormData>({
+    firstName: '',
+    lastName: '',
+    petChoice: '',
+    petChoice2: '',
+  });
+
+  const handleChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>
+  ) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const onSubmitHandle = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    console.log('Form Data:', formData);
+  };
+
+  return (
+    <main className='flex flex-col items-center'>
+      <h1 className='text-4xl font-bold my-4'> Welcome to A/B Testing</h1>
+
+      <section>
+        <h2 className='text-2xl text-center my-2'>Personality test</h2>
+
+        <form onSubmit={onSubmitHandle}>
+          <div className='pb-12'>
+            <h2 className='text-base font-semibold leading-7 text-gray-900'>
+              Personal Information
+            </h2>
+            <p className='mt-1 text-sm leading-6 text-gray-600'>
+              Let's see what kind of person you are.
+            </p>
+
+            <div className='mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6'>
+              <div className='sm:col-span-3'>
+                <label
+                  htmlFor='firstName'
+                  className='block text-sm font-medium leading-6 text-gray-900'
+                >
+                  First name
+                </label>
+                <div className='mt-2'>
+                  <input
+                    type='text'
+                    name='firstName'
+                    id='firstName'
+                    onChange={handleChange}
+                    className='block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6'
+                  />
+                </div>
+              </div>
+
+              <div className='sm:col-span-3'>
+                <label
+                  htmlFor='lastName'
+                  className='block text-sm font-medium leading-6 text-gray-900'
+                >
+                  Last name
+                </label>
+                <div className='mt-2'>
+                  <input
+                    type='text'
+                    name='lastName'
+                    id='lastName'
+                    onChange={handleChange}
+                    className='block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6'
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <fieldset>
+            <legend className='text-sm font-semibold leading-6 text-gray-900'>
+              Are you a dog person or a cat person?
+            </legend>
+            <div className='mt-6 space-y-6'>
+              <div className='flex items-center gap-x-3'>
+                <input
+                  id='dogs'
+                  value='dogs'
+                  checked={formData['petChoice'] === 'dogs'}
+                  onChange={handleChange}
+                  name='petChoice'
+                  type='radio'
+                  className='h-4 w-4 border-gray-300 text-indigo-600 focus:ring-indigo-600'
+                />
+                <label
+                  htmlFor='dogs'
+                  className='block text-sm font-medium leading-6 text-gray-900'
+                >
+                  Dogs of course, they're so clever!
+                </label>
+              </div>
+              <div className='flex items-center gap-x-3'>
+                <input
+                  id='cats'
+                  value='cats'
+                  checked={formData['petChoice'] === 'cats'}
+                  onChange={handleChange}
+                  name='petChoice'
+                  type='radio'
+                  className='h-4 w-4 border-gray-300 text-indigo-600 focus:ring-indigo-600'
+                />
+                <label
+                  htmlFor='cats'
+                  className='block text-sm font-medium leading-6 text-gray-900'
+                >
+                  There's nothing like the purr of a cat!
+                </label>
+              </div>
+              <div className='flex items-center gap-x-3'>
+                <input
+                  id='none'
+                  value='allergic'
+                  checked={formData['petChoice'] === 'allergic'}
+                  onChange={handleChange}
+                  name='petChoice'
+                  type='radio'
+                  className='h-4 w-4 border-gray-300 text-indigo-600 focus:ring-indigo-600'
+                />
+                <label
+                  htmlFor='none'
+                  className='block text-sm font-medium leading-6 text-gray-900'
+                >
+                  I'm allergic.
+                </label>
+              </div>
+            </div>
+
+            {/* ------------------------------------------------------------------- */}
+
+            <div className='mt-6 space-y-6 flex items-center gap-x-3'>
+              <div className='sm:col-span-3'>
+                <label
+                  htmlFor='petChoice2'
+                  className='block text-sm font-medium leading-6 text-gray-900'
+                >
+                  Are you a dog person or a cat person?
+                </label>
+                <div className='mt-2'>
+                  <select
+                    onChange={handleChange}
+                    id='petChoice2'
+                    name='petChoice2'
+                    // value={formData['petChoice2']}
+                    className='block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6'
+                  >
+                    <option>---chose answer---- </option>
+                    <option value='dogs'>
+                      DOGS of course, they're so clever!
+                    </option>
+                    <option value='cats'>
+                      There's nothing like the purr of a CAT!
+                    </option>
+                    <option value='allergic'>I'm allergic.</option>
+                  </select>
+                </div>
+              </div>
+            </div>
+          </fieldset>
+
+          <div className='mt-6 flex items-center justify-center'>
+            <button
+              type='submit'
+              className='rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600'
+            >
+              Save
+            </button>
+          </div>
+        </form>
+      </section>
+    </main>
+  );
+};
+
+export default App;
